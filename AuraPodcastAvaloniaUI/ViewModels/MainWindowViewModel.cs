9be2ad814646599f7c96e08489754b1f16a04949
Cactus <amyfor7@gmail.com>
2025-07-06 17:27:56 +0800

    feat: add avalonia ui

---
+++ AuraPodcastAvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using AuraPodcastAvaloniaUI.Models;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AuraPodcastAvaloniaUI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private static readonly HttpClient _httpClient = new HttpClient();
+
+    public ObservableCollection<WeatherForecast> Forecasts { get; } = new();
+
+    [RelayCommand]
+    private async Task LoadForecastsAsync()
+    {
+        Forecasts.Clear();
+
+        try
+        {
+            var apiUrl = "https://localhost:7094/WeatherForecast";
+
+            var forecasts = await _httpClient.GetFromJsonAsync<WeatherForecast[]>(apiUrl);
+
+            if (forecasts != null)
+            {
+                foreach (var forecast in forecasts)
+                {
+                    Forecasts.Add(forecast);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching data: {ex.Message}");
+        }
+    }
+}
---
+++ AuraPodcastAvaloniaUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AuraPodcastAvaloniaUI.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
